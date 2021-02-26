ea468965f7ce344893be361025424367728a4a2c
jblanava <0619836326@uma.es>
2021-02-26 12:07:31 +0100

    final1

--- botpython.py
+++ botpython.py
@@ -5,6 +5,38 @@ import json
 from ibm_watson import AssistantV1
 from ibm_watson import AssistantV2
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import pymongo
+from decouple import config
+import os
+
+
+client = pymongo.MongoClient(config('MONGO_URI'))
+mydb = client["BobiDB"]
+
+
+
+def isInDB(nombre):
+    mycol = mydb["Usuario"]
+    query = {"nombre": nombre}
+    mydoc = mycol.find_one(query)
+    esta = False
+    
+    if mydoc == None:
+        esta = False
+    else:
+        esta = True
+    return esta
+
+def InsertInDB(nombre):
+    mycol = mydb["Usuario"]
+    if nombre is not None and not isInDB(nombre):
+        mydict = {"nombre" : nombre}
+        mycol.insert_one(mydict)
+def DeleteDB(nombre):
+    mycol = mydb["Usuario"]
+    if nombre is not None and isInDB(nombre):
+        query = {"nombre" : nombre}
+        mycol.delete_one(query)
 
 def normalize(s):
     replacements = (
@@ -25,7 +57,6 @@ class BotHandler:
             self.token = token
             self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
-    #url = "https://api.telegram.org/bot<token>/"
 
     def get_updates(self, offset=0, timeout=30):
         method = 'getUpdates'
@@ -51,28 +82,28 @@ class BotHandler:
         return last_update
 
 
-token = '1680332449:AAGzfEsLEGjTfbHcI-tALelZ4OUwKNgG1zs' #Token of your bot
+token = config('TOKEN_BOT') #Token of your bot
 magnito_bot = BotHandler(token) #Your bot's name
 
 
 def main():
     new_offset = 228565091
     print('hi, now launching...')
-    authenticator = IAMAuthenticator('ITvi-zv-b3OBJ-4CWFOVM7954tQA48IZlB10k3MDhrtF')
+    authenticator = IAMAuthenticator(config('AUTHENTICATOR'))
     assistant = AssistantV2(
         version='2020-04-01',
         authenticator = authenticator
     )
 
-    assistant.set_service_url('https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/051b267d-12bb-4cc6-9282-ff649966c3f8')
+    assistant.set_service_url(config('ASSISTANT_URL'))
 
     responseCreate = assistant.create_session(
-        assistant_id='e62c449c-c2a0-4c9d-a5c7-573f5a907c0c'
+        assistant_id=config('ASSISTANT_ID')
     ).get_result()
 
     
     while True:
-        #try: 
+        try: 
             all_updates=magnito_bot.get_updates(new_offset)
 
             if len(all_updates) > 0:
@@ -96,16 +127,23 @@ def main():
                         
                     if first_chat_text != '/start':  # do nothing
                         response = assistant.message(
-                            assistant_id='e62c449c-c2a0-4c9d-a5c7-573f5a907c0c',
+                            assistant_id=config('ASSISTANT_ID'),
                             session_id=responseCreate['session_id'],
                             input={
                             'message_type': 'text',
                             'text': normalize(first_chat_text),
-                            }
+                            },
+                            user_id=first_chat_id,
                         ).get_result()
                     
-                        #print(json.dumps(response, indent=2))
                         print(json.dumps(response, indent = 2))
+
+                        intent = response['output']['intents'][0]['intent']
+
+                        if intent == 'Subscripcion':
+                            InsertInDB(response['user_id'])    
+                        elif intent == 'Desubscripcion':
+                            DeleteDB(response['user_id']) 
 
                         magnito_bot.send_message(first_chat_id, json.dumps(normalize(response['output']['generic'][0]['text']), indent=2))
                         new_offset = first_update_id + 1
@@ -116,13 +154,9 @@ def main():
 
                         
 
-                        '''
-                        responseLista = assistant.list_intents(
-                            workspace_id='1a37b021-5a0a-479d-a7e5-b2adfab8b256'
-                        ).get_result()
-                        '''
-       # except:
-            #print("Ha habido un error")
+                        
+        except:
+            print("Ha habido un error")
 
                
 
